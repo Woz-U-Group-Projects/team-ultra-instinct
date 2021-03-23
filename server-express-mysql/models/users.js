18d7e241c55010c34659ec161c6d5ab07d7a074c
dmcdaniel89 <69585344+dmcdaniel89@users.noreply.github.com>
2021-03-22 21:41:05 -0500

    full auth changes

--- server-express-mysql/models/users.js
+++ server-express-mysql/models/users.js
@@ -1,20 +1,9 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  users.init({
-    UserId:  {
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('users', {
+    UserId: {
+      type: DataTypes.INTEGER(11),
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
@@ -37,5 +26,4 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'users',
   });
-  return users;
 };
